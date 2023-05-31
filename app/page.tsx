5d849b7c299e1eed89aa41a13c7733d03538e38c
spineda1208 <spineda1208@gmail.com>
2023-05-31 17:41:17 -0600

    Minor text changes

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ export default function HomePage() {
   return (
     <>
     <section className='mb-8'>
-{/* Identification. Tracking. Efficency. */}
+    {/* Identification. Tracking. Efficency. */}
     <div className="custom-shape-top">
     <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
         <path d="M892.25 114.72L0 0 0 120 1200 120 1200 0 892.25 114.72z" className="shape-fill"></path>
