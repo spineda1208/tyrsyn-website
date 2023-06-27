a9aa33805e2c4454d79bde40f92ace84b71ad2a1
spineda1208 <spineda1208@gmail.com>
2023-06-26 20:05:52 -0600

    Home and Contact Page Updates

--- app/contactus/page.tsx
+++ app/contactus/page.tsx
@@ -1,5 +1,6 @@
 /* eslint-disable react/no-unescaped-entities */
 "use client"
+import { useRef } from 'react'
 import { FormEvent, useState } from 'react';
 import {MdInbox, MdMail, MdPhone, MdAccountCircle} from 'react-icons/md'
 
@@ -31,8 +32,17 @@ export default function ContactPage() {
 
   function handleSubmit(e: FormEvent) {
     e.preventDefault()
-    console.log(data)
+    console.log(data) //Connect to API here
   }
+
+  const inputRef = useRef<HTMLElement>(null);
+
+  const inputTextAnimation = () => {
+        // inputRef.current.hasAttribute('data-visible') ? navToggle.setAttribute('aria-expanded'. true) : console.log('false')
+        if (inputRef.current) {
+            inputRef.current.toggleAttribute('data-visible')
+        };
+  };
 
   return (
       <>
@@ -45,28 +55,28 @@ export default function ContactPage() {
             </div>
             <form onSubmit={handleSubmit} action="" method="post" className="contact-form">
               <div className="contact-input-wrapper">
-                <input value={data.firstName} className="contact-input" autoComplete="given-name" name="First Name" type="text" autoFocus required onChange={e => updateData({firstName :e.target.value})}/>
-                <label>First Name</label>
+                <input value={data.firstName} className="contact-input" autoComplete="given-name" name="First Name" type="text" required onChange={e => updateData({firstName :e.target.value})}/>
+                <label className='group-focus:translate-y-1'>First Name</label>
                 <MdAccountCircle/>
               </div>
               <div className="contact-input-wrapper">
-                <input value={data.lastName} className="contact-input" autoComplete="family-name" name="Last Name" type="text" required onChange={e => updateData({lastName :e.target.value})}/>
-                <label>Last Name</label>
+                <input value={data.lastName} className="contact-input group" autoComplete="family-name" name="Last Name" type="text" required onChange={e => updateData({lastName :e.target.value})}/>
+                <label className='group-focus:text-green-700'>Last Name</label>
                 <MdAccountCircle/>
               </div>
               <div className="contact-input-wrapper">
-                <input value={data.email} className="contact-input" autoComplete="email" name="Email" type="text" required onChange={e => updateData({email :e.target.value})}/>
-                <label>Email</label>
+                <input value={data.email} className="contact-input group" autoComplete="email" name="Email" type="text" required onChange={e => updateData({email :e.target.value})}/>
+                <label className='group-focus:text-green-700'>Email</label>
                 <MdMail/>
               </div>
               <div className="contact-input-wrapper">
-                <input value={data.phone} className="contact-input" autoComplete="tel-national" name="Phone" type="text" required onChange={e => updateData({phone :e.target.value})}/>
-                <label>Phone</label>
+                <input value={data.phone} className="contact-input group" autoComplete="tel-national" name="Phone" type="text" required onChange={e => updateData({phone :e.target.value})}/>
+                <label className='group-focus:text-green-700'>Phone</label>
                 <MdPhone/>
               </div>
               <div className="contact-input-wrapper-full large">
-                <textarea value={data.message} name="Message" autoComplete="off" id="" cols={30} rows={10} className="contact-input" required onChange={e => updateData({message :e.target.value})}></textarea>
-                <label>Message</label>
+                <textarea value={data.message} name="Message" autoComplete="off" id="" cols={30} rows={10} className="contact-input group" required onChange={e => updateData({message :e.target.value})}></textarea>
+                <label className='group-focus:text-green-700'>Message</label>
                 <MdInbox/>
               </div>
               <button type='submit' className='hero-button'>Submit</button>
